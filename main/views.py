3f8b42ff056aabe66b9953a0885991057dbfc7ef
aliyurekli32 <adamjesperblack@gmail.com>
2022-12-25 16:23:29 +0300

    Reset: urls and views of student api

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import HttpResponse
 
 def real_home(request):
-    return HttpResponse('This is main HomePage..')
+    return HttpResponse('<a href="http://127.0.0.1:8000/admin">This is admin link</a><a href="http://127.0.0.1:8000/path">This is path link</a><a href="http://127.0.0.1:8000/api">This is api link</a>')
